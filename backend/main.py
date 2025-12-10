fbf02faa3c27903c06ab492e044b834bae93a289
Ka1laK <rissel1771@gmail.com>
2025-12-09 21:23:11 -0500

    fix(backend): remove wildcard cors origin to support credentials

--- backend/main.py
+++ backend/main.py
@@ -75,8 +75,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://ka1lak.github.io",
-        "*"
+        "https://ka1lak.github.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
